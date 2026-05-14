31d32dc2d2b9b0f7fac9ec27505ef1cc711013e7
loicsikidi <loic.sikidi@gmail.com>
2026-05-14 23:35:05 +0200

    feat(goutils): add Ptr and Derefence funcs

--- common.go
+++ common.go
@@ -9,3 +9,17 @@ func Copy[T any](v *T) *T {
 	copied := *v
 	return &copied
 }
+
+// Ptr returns a pointer to the given value.
+func Ptr[T any](v T) *T {
+	return &v
+}
+
+// Dereference returns the value pointed to by v, or the zero value if v is nil.
+func Dereference[T any](v *T) T {
+	if v == nil {
+		var zero T
+		return zero
+	}
+	return *v
+}
